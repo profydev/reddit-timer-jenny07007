d40f79525059a8438e18096298c43dc1838bd2df
jenny07007 <jennyliu07@gmail.com>
2021-04-17 05:03:43 +0800

    change to self-hosting fonts

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,6 +1,44 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff2") format("woff2"),
+    url("/fonts/Montserrat-Regular.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff2") format("woff2"),
+    url("/fonts/Montserrat-Medium.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff2") format("woff2"),
+    url("/fonts/Montserrat-SemiBold.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 700;
+    font-style: normal;
+    src: url("Montserrat-Bold.woff2") format("woff2"),
+    url("Montserrat-Bold.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Bitter";
+    src: url("/fonts/Bitter-Regular.woff2") format("woff2"),
+    url("/fonts/Bitter-Regular.woff") format("woff");
+  }
+
   html, body{
     font-family: 'Bitter', 'Montserrat',serif;
     -webkit-font-smoothing: antialiased;
