64b92007d111ddee3573028bf97ea29560f606b2
jenny07007 <jennyliu07@gmail.com>
2021-04-19 12:13:59 +0800

    fix redundant css code and undefined theme property. optimize svg with SVGOMG

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 import App from './App';
 
 test('renders App', () => {
-  render(<App />);
+  render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+  );
 });
--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -11,13 +11,11 @@ export const Container = styled.header`
 
 export const Logo = styled.img`
   margin-left: 80px;
-  width: 150px;
-  height: 36px;
 `;
 
 export const NavLink = styled(Link)`
-  font-size: ${(props) => props?.theme?.font?.size?.default};
-  color: ${(props) => props?.theme?.color?.gray};
+  font-size: ${(props) => props.theme.font.size.default};
+  color: ${(props) => props.theme.color.gray};
   text-decoration: none;
   margin-left: 25px;
 `;
