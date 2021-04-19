c831b25159f7c197be1fc03dd7dd8aec6f3c62fd
jenny07007 <jennyliu07@gmail.com>
2021-04-20 00:53:26 +0800

    fix lint error in testing

--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-
 export const Container = styled.header`
   height: 100px;
   width: 100vw;
