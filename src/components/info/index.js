3e99636584e3451315e67992d836c984b68dc078
jenny07007 <jennyliu07@gmail.com>
2021-05-01 14:46:35 +0800

    reshape structure

--- src/components/info/index.js
+++ src/components/info/index.js
@@ -1,44 +1,28 @@
 import React from 'react';
-import {
-  Container, Row, Title, ListText, Text, Link, Box,
-} from './info.style';
+import InfoSection from './info-section';
+import { Section, Link } from './info.style';
 
 export default function Info() {
   return (
-    <Container>
-      <Row>
-        <Box>
-          <Title id="how-it-works">How it works</Title>
-          <ListText>
-            &#183; We find the 500 top posts from the past year for a subreddit
-          </ListText>
-          <ListText>
-            &#183; The data is visualized in a heatmap grouped by weekday and hour
-            of the day.
-          </ListText>
-          <ListText>
-            &#183; See immediately when to submit your reddit post.
-          </ListText>
-        </Box>
-
-        <Box>
-          <Title id="about">About</Title>
-          {/* &#x2011; for non-breaking hyphen */}
-          <Text>
-            This app was created during a course on
-            {' '}
-            <Link href="https://profy.dev">profy.dev</Link>
-            {' '}
-            with the goal toimplement a pixel&#x2011;perfect real-world application with
-            professional workflows and tools like Kanban, Asana, Zeplin, GitHub,
-            pull requests and code reviews.
-            <Link href="https://profy.dev/employers">
-              {' '}
-              Click here for more information.
-            </Link>
-          </Text>
-        </Box>
-      </Row>
-    </Container>
+    <Section>
+      <InfoSection id="how-it-works" headline="How it works">
+        • We find the 500 top posts from the past year for a subreddit.
+        <br />
+        • The data is visualized in a heatmap grouped by weekday and hour of the day.
+        <br />
+        • See immediately when to submit your reddit post.
+        <br />
+      </InfoSection>
+      <InfoSection id="about" headline="About">
+        This app was created during a course on
+        {' '}
+        <Link href="https://profy.dev">profy.dev</Link>
+        {' '}
+        with the goal to implement a pixel&#x2011;perfect real-world application with professional
+        workflows and tools like Kanban, Asana, Zeplin, GitHub, pull requests and code reviews.
+        {' '}
+        <Link href="https://profy.dev/employers">Click here for more information.</Link>
+      </InfoSection>
+    </Section>
   );
 }
--- src/components/info/info.style.js
+++ src/components/info/info.style.js
@@ -1,55 +1,35 @@
 import styled, { css } from 'styled-components/macro';
 
-export const Container = styled.section`
-  width: 100vw;
-  margin-bottom: 6.3rem;
+export const Section = styled.section`
+  width: 100%;
+  max-width: 778px;
+  margin: 122px auto 0;
+  padding: 0 28px;
 `;
 
-export const Row = styled.article`
-  max-width: 738px;
-  margin: 0 auto;
- 
-  @media (max-width: 738px) {
-    width: 90%;
-  }
-`;
-
-export const Box = styled.div`
-  padding-top: 6.8rem;
-
-
-  &:nth-child(2) {
-    padding-top: 4.6rem;
-  }
+export const Article = styled.article`
+  margin: 100px 0;
 `;
 
 const TextBase = css`
   color: ${({ theme }) => theme.color.lightGray};
   font-weight: ${({ theme }) => theme.font.weight.normal};
   font-family: ${({ theme }) => theme.font.family.default};
+  font-size: ${({ theme }) => theme.font.size.default};
 `;
 
-export const Title = styled.h3`
+export const Headline = styled.h3`
   ${TextBase};
   font-size: ${({ theme }) => theme.font.size.h3};
   font-family: ${({ theme }) => theme.font.family.headline};
   color: ${({ theme }) => theme.color.black};
+  margin: 0;
+  line-height: 1.9; 
 `;
 
-export const ListText = styled.p`
-  ${TextBase};
-  margin-top: -4px;
-  line-height: 0.95;
-
-  @media (max-width: 738px) {
-    line-height: 1.7;
-  }
-`;
-
-export const Text = styled.p`
+export const Content = styled.div`
   ${TextBase}
-  margin-top: -8px;
-  line-height: 1.7;
+  line-height: 1.8; 
 `;
 
 export const Link = styled.a`
